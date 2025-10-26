b7c9115f8d28fcdfe56d53bdc9bda4533963aeab
Faizan Alam <faizansaikh786786f@gmail.com>
2025-10-26 19:58:28 +0530

    Hackathon

--- Backend/server.js
+++ Backend/server.js
@@ -5,19 +5,29 @@ const authroutes = require("./routes/authroutes")
 const Habitroutes = require('./routes/habitRoutes')
 const habitlog = require('./routes/logroutes')
 const goalroutes = require('./routes/goalRoutes')
-const scheduler = require('./utils/scheduler')
+// const scheduler = require('./utils/scheduler')
 const soulroutes = require("./routes/soulFuelRoutes")
 const notificationroutes = require("./routes/notificationRoutes")
 const analytics = require("./routes/analyticsRoutes")
 const app = express()
 app.use(express.json())
+
 const PORT = process.env.PORT || 4300
 const cors = require('cors')
 const path = require("path")
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(cors())
+connectDB()
 
-scheduler.init()
+// scheduler.init()
+app.get("/", (req, res) => {
+    try {
+      res.status(201).send({ message: "Hello, world!" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  })
 app.use('/soulfuel',soulroutes );
 app.use('/notifications', notificationroutes);
 app.use('/analytics', analytics)
@@ -25,10 +35,21 @@ app.use("/goal" , goalroutes)
 app.use("/habitLog" , habitlog)
 app.use("/users" , authroutes)
 app.use("/habit" , Habitroutes)
-connectDB()
-app.listen(PORT, ()=>{
-    console.log(`server running at http://localhost:${PORT}`)
-})
+
+// app.listen(PORT, ()=>{
+//     console.log(`server running at http://localhost:${PORT}`)
+// })
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
 
 
 
